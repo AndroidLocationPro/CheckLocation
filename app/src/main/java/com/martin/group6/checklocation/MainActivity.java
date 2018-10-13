b70ce60d582d52331f08dfefd3fba14ae3076149
Martin <15037887317@163.com>
2018-10-13 13:36:35 +0800

    与服务器端通信

--- app/src/main/java/com/martin/group6/checklocation/MainActivity.java
+++ app/src/main/java/com/martin/group6/checklocation/MainActivity.java
@@ -1,13 +1,110 @@
 package com.martin.group6.checklocation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    EditText usernameEt;
+    EditText passwordEt;
+    Button loginBt;
+    private String responseMsg = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usernameEt = findViewById(R.id.usernameEt);
+        passwordEt = findViewById(R.id.passwordEt);
+        loginBt = findViewById(R.id.loginBt);
+        loginBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameEt.getText().toString();
+                String password = passwordEt.getText().toString();
+                LoginRequest("张三","admin");
+            }
+        });
     }
+
+
+    public void LoginRequest(final String accountNumber, final String password) {
+        //请求地址
+        String url = "http://58.87.113.82:8080/TalkToAndroid2/LoginServlet";    //注①
+        String tag = "Login";    //注②
+
+        //取得请求队列
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        //防止重复请求，所以先取消tag标识的请求队列
+        requestQueue.cancelAll(tag);
+
+        //创建StringRequest，定义字符串请求的请求方式为POST(省略第一个参数会默认为GET方式)
+        final StringRequest request = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = (JSONObject) new JSONObject(response).get("params");  //注③
+                            String result = jsonObject.getString("Result");  //注④
+                            if (result.equals("success")) {  //注⑤
+                                startActivity(new Intent(MainActivity.this,Main2Activity.class));
+                            } else {
+                                startActivity(new Intent(MainActivity.this,Main3Activity.class));
+                            }
+                        } catch (JSONException e) {
+                            Toast.makeText(MainActivity.this,"jaso数据交换失败",Toast.LENGTH_SHORT).show();
+                            Log.e("TAG", e.getMessage(), e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //做自己的响应错误操作，如Toast提示（“请稍后重试”等）
+                Toast.makeText(MainActivity.this,"volley响应错误",Toast.LENGTH_SHORT).show();
+                Log.e("TAG", error.getMessage(), error);
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("AccountNumber", accountNumber);  //注⑥
+                params.put("Password", password);
+                return params;
+            }
+        };
+
+        //设置Tag标签
+        request.setTag(tag);
+
+        //将请求添加到队列中
+        requestQueue.add(request);
+    }
+
 }
